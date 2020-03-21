23f81fc146415cb707a7445fe6805057db1b3da3
Joel Duncan <joel@slethen.io>
2020-03-21 04:48:28 +0000

    Change header image

--- advise.php
+++ advise.php
@@ -18,7 +18,7 @@
 
 <style>
   .jumbotron {
-  background-image: url(https://www.charlescountymd.gov/Home/ShowPublishedImage/4496/637195158875230000);
+  background-image: url(https://joelduncan.io/content/images/size/w2000/2020/03/coronavirus-header.jpg);
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
