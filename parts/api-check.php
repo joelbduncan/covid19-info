4bfa56abf5211de79b92b1f07f0099b164e4cbff
Joel Duncan <joel@slethen.io>
2020-06-19 01:35:13 +0100

    Use alt API URL, add link to current API

--- parts/api-check.php
+++ parts/api-check.php
@@ -6,27 +6,28 @@ $apiMain = "https://api.covid-19.UK.com/countries/UK";
 $apiMainJson = json_decode(file_get_contents($apiMain), true);
 
 // Check UK Today cases on alternative API
-$apiBackup = "https://corona.lmao.ninja/v2/countries/UK";
+$apiBackup = "https://disease.sh/v2/countries/UK";
 $apiBackupJson = json_decode(file_get_contents($apiBackup), true);
 
 // Use self hosted API unless alternative has more cases
 if ($apiMainJson["todayCases"] > $apiBackupJson["todayCases"]){
     $apiURL = "https://api.covid-19.UK.com";
     $yesterdayApiURL = "https://api.covid-19.UK.com/yesterday/$selectCountry";
-    $currentAPI = "Main";
+    $currentAPI = '<a href="https://api.covid-19.uk.com/">Main</a>';
   }
   // Use self hosted API when numbers are equal
   elseif ($apiMainJson["todayCases"] == $apiBackupJson["todayCases"]){
       $apiURL = "https://api.covid-19.UK.com";
       $yesterdayApiURL = "https://api.covid-19.UK.com/yesterday/$selectCountry";
       $twoDayApiURL = "https://api.covid-19.UK.com/twoDay/$selectCountry";
-      $currentAPI = "Main";
+      $currentAPI = '<a href="https://api.covid-19.uk.com/">Main</a>';
     }
   else {
       // Use backend in other scenarios
-      $apiURL = "https://corona.lmao.ninja/v2";
-      $yesterdayApiURL = "https://corona.lmao.ninja/v2/countries/$selectCountry?yesterday=true";
-      $currentAPI = "Backup";
+      $apiURL = "https://disease.sh/v2";
+      $yesterdayApiURL = "https://disease.sh/v2/countries/$selectCountry?yesterday=true";
+      $twoDayApiURL = "https://disease.sh/v2/countries/$selectCountry?twoDaysAgo=true";
+      $currentAPI = '<a href="https://disease.sh/">disease.sh</a>';
   }
 
 ?>
